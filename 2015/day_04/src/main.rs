ccbe7feb973f772ef96c9949428af9228e507316
kyleeh90 <123283545+kyleeh90@users.noreply.github.com>
2024-08-02 19:53:29 +0000

    feat: complete day 4 2015
      - finish and verify day 4 2015 results

--- 2015/day_04/src/main.rs
+++ 2015/day_04/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+// Using md5 for hashing
+use md5;
+
+// Base of key string
+const KEY_BASE: &str = "yzbqklnj";
+
+fn main(){
+    // Setup necessary variables
+    let mut current_number: u64 = 1;
+
+    let mut hash: String = String::new();
+
+    // Loop until the below criteria is met
+    // The input to the MD5 hash is some secret key (your puzzle input, given below) followed by a number in decimal. 
+    // To mine AdventCoins, you must find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+
+    // Part two Now find one that starts with six zeroes.
+    while !hash.starts_with("000000"){
+        // Compute a new hash
+        hash = format!("{:x}", md5::compute(format!("{}{}", KEY_BASE, current_number)));
+
+        // Increment number
+        current_number += 1;
+    }
+
+    println!("Lowest Positive Number That Begins With 6 Zeroes: {}", current_number - 1)
 }
